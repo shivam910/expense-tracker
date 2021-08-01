e6cc721a1585740eff255bb96463712d1de01678
shivam910 <shivams91099@gmail.com>
2021-08-01 18:59:55 +0530

    react

---
+++ src/components/TransactionList.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Transaction } from "./Transaction";
+
+export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  {
+    return (
+      <div>
+        <h3>History</h3>
+        <ul id="list" className="list">
+          {transactions.map((transaction) => (
+            <Transaction key={transaction.id} transaction={transaction} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+};
